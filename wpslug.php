e2d058afd9ac5d4493d897a8758a06f4e06ed247
wenpai <wenpai@feibisi.com>
2026-03-11 16:48:21 +0800

    fix: WPSLUG_VERSION 常量更新为 1.2.0

--- wpslug.php
+++ wpslug.php
@@ -19,7 +19,7 @@ if (!defined("ABSPATH")) {
     exit();
 }
 
-define("WPSLUG_VERSION", "1.1.1");
+define("WPSLUG_VERSION", "1.2.0");
 define("WPSLUG_PLUGIN_DIR", plugin_dir_path(__FILE__));
 define("WPSLUG_PLUGIN_URL", plugin_dir_url(__FILE__));
 define("WPSLUG_PLUGIN_BASENAME", plugin_basename(__FILE__));
